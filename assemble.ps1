4f25cb92800815ba9227d694c0946e2d3a5350c3
Matthias <aussieevil@hotmail.com>
2022-08-15 09:54:56 -0700

    big stinky api macro

--- assemble.ps1
+++ assemble.ps1
@@ -1,2 +1,2 @@
 ca65 .\asm\ignition.a65 -l .\listings\ignition.txt -o .\bin\ignition.o
-#ca65 .\sysman.a65 -l .\listings\sysman.txt -o .\bin\sysman.o
+ca65 .\sysman.a65 -l .\listings\sysman.txt -o .\bin\sysman.o
