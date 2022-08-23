0e9df7e8cc7e3f49fbd12fde22dbfe768ba90060
Matthias <aussieevil@hotmail.com>
2022-08-23 01:54:49 -0700

    bit of work on Ignition

--- platform/Dragon816/make.ps1
+++ platform/Dragon816/make.ps1
@@ -1,2 +1,2 @@
 ca65 .\dragon816.a65 -d -g -l dragon816-listing.txt
-ld65 -C .\dragon816.cfg  -v -vm -m map.txt .\dragon816.o
+ld65 -C .\dragon816.cfg  -v -vm -m map.txt .\dragon816.o ..\..\bin\ignition.o
